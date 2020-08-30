834af896403f079f37240887dce711968734f7b6
Otávio Ramos Zanelatto <54379834+otavioZane@users.noreply.github.com>
2020-08-30 20:03:16 -0300

    Update TesteTecnicoSqlOtavio.sql

--- TesteTecnicoSqlOtavio.sql
+++ TesteTecnicoSqlOtavio.sql
@@ -1,19 +1,24 @@
 
--- Com base no modelo acima, escreva um comando SQL que liste a quantidade de registros por status com sua descrição.
+-- Com base no modelo acima, escreva um comando SQL que liste a quantidade de registros por status com sua descriÃ§Ã£o.
+
 select dsStatus, count (dsStatus) as Quantia from tb_Status 
 group by dsStatus 
 
--- Com base no modelo acima, construa um comando SQL que liste a maior data de andamento por número de processo, com processos encerrados no ano de 2013.
+-- Com base no modelo acima, construa um comando SQL que liste a maior data de andamento por nÃºmero de processo, com processos encerrados no ano de 2013.
+
 select max(tb_Andamento.dtAndamento) as maior_andamento from tb_Andamento
 inner join tb_Processo on tb_Processo.idProcesso = tb_Andamento.idProcesso
 where tb_Processo.dtEncerramento <= '2013-12-31'
 
--- Com base no modelo acima, construa um comando SQL que liste a quantidade de Data de Encerramento agrupada por ela mesma com a quantidade da contagem seja maior que 5.select dtEncerramento, count(dtEncerramento) as quantia
+-- Com base no modelo acima, construa um comando SQL que liste a quantidade de Data de Encerramento agrupada por ela mesma com a quantidade da contagem seja maior que 5.
+
+select dtEncerramento, count(dtEncerramento) as quantia
 from tb_Processo
 group by dtEncerramento having count(dtEncerramento) < 5;
 
--- 4. Possuímos um número de identificação do processo, onde o mesmo contem 12 caracteres com zero à esquerda, contudo nosso modelo e dados ele é apresentado como bigint. Como 
+-- 4. PossuÃ­mos um nÃºmero de identificaÃ§Ã£o do processo, onde o mesmo contem 12 caracteres com zero Ã  esquerda, contudo nosso modelo e dados ele Ã© apresentado como bigint. Como 
 -- fazer para apresenta-lo com 12 caracteres considerando os zeros a esquerda?
+
 select right('00000000000' + cast(nroProcesso as varchar(12)), 12) from tb_Processo
 where idProcesso = 1
 
